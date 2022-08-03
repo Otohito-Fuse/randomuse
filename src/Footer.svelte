ddf10c5680de7945dfc9804f9a689e9f246aec83
Otohito-Fuse <otohitofuse@gmail.com>
2022-08-03 16:17:12 +0900

    updated footer

--- src/Footer.svelte
+++ src/Footer.svelte
@@ -6,7 +6,7 @@
     footer {
         text-align: center;
         margin-top: 15px;
-        padding: 15px 0;
+        padding: 12px 0 18px 0;
         box-shadow: 0px -5px 10px 0px rgba(0, 0, 0, 0.2);
     }
 
